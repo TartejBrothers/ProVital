bfbbfc9f2f83e46679e0ac0076ce2f22dbbd17f3
Taranjeet Singh Bedi <taranjeetsinghbedi2@gmail.com>
2024-12-06 21:56:34 +0530

    Added: Search Component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,60 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import "../styles/home.css";
+import Homeleftcolumnone from "../images/hometop/homeleftone1.jpg";
+import Homeleftcolumntwo from "../images/hometop/homeleftone2.jpg";
+import Homeleftcolumnthree from "../images/hometop/homeleftone3.jpg";
+import Homeleftcolumnfour from "../images/hometop/homeleftone4.jpg";
+import Homerightcolumnone from "../images/hometop/homelefttwo1.jpg";
+import Homerightcolumntwo from "../images/hometop/homelefttwo2.jpg";
+import Homerightcolumnthree from "../images/hometop/homelefttwo3.jpg";
+import Homerightcolumnfour from "../images/hometop/homelefttwo4.jpg";
+import Search from "../components/search";
 
 export default function Home() {
   return (
     <div className="homemain">
       <Navbar />
+      <div className="hometop">
+        <div className="hometopleft">
+          <div className="homeleftcolumonemain">
+            <div className="homeleftcolumnone">
+              <img src={Homeleftcolumnone} alt="One" />
+              <img src={Homeleftcolumntwo} alt="Two" />
+              <img src={Homeleftcolumnthree} alt="Three" />
+              <img src={Homeleftcolumnfour} alt="Four" />
+            </div>
+            <div className="homeleftcolumnone">
+              <img src={Homeleftcolumnone} alt="One" />
+              <img src={Homeleftcolumntwo} alt="Two" />
+              <img src={Homeleftcolumnthree} alt="Three" />
+              <img src={Homeleftcolumnfour} alt="Four" />
+            </div>
+          </div>
+          <div className="homeleftcolumonemain">
+            <div className="homeleftcolumntwo">
+              <img src={Homerightcolumnone} alt="One" />
+              <img src={Homerightcolumntwo} alt="Two" />
+              <img src={Homerightcolumnthree} alt="Three" />
+              <img src={Homerightcolumnfour} alt="Four" />
+            </div>
+            <div className="homeleftcolumntwo">
+              <img src={Homerightcolumnone} alt="One" />
+              <img src={Homerightcolumntwo} alt="Two" />
+              <img src={Homerightcolumnthree} alt="Three" />
+              <img src={Homerightcolumnfour} alt="Four" />
+            </div>
+          </div>
+        </div>
+        <div className="hometopright">
+          <h1>
+            Book an appointment with{" "}
+            <span className="gradientcolor">lifestyle medicine</span> experts
+          </h1>
+          <p>Optimize your lifestyle and reverse chronic diseases.</p>
+        </div>
+      </div>
+      <Search />
     </div>
   );
 }
